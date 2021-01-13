94e29e7e9a7389f6295d4b50df60a7983c27834c
afcastaneda223 <af.castaneda223@gmail.com>
2021-01-13 17:38:54 -0300

    added posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { minimum: 5, maximum: 500 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :name, presence: true, uniqueness: true
 end
